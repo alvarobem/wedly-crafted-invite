6aa4ef525a3e7ffc8c4500cf3464575eb2f68dc6
Alvaro Berrocal <alvaro.berrocal@smartpadel.com>
2025-08-11 16:36:35 +0200

    Updates

--- supabase/migrations/20250806073508_353ea632-699d-43c7-a7cb-6a8727853ca3.sql
+++
@@ -1,4 +0,0 @@
--- Add columns for dietary restrictions and special notes to guests table
-ALTER TABLE public.guests 
-ADD COLUMN dietary_restrictions TEXT,
-ADD COLUMN special_notes TEXT;
--- supabase/migrations/20250810111937_b1e9c13e-eef8-4521-827d-2991751593c8.sql
+++
@@ -1,3 +0,0 @@
--- Permitir que la columna group_name sea nullable
-ALTER TABLE public.guests 
-ALTER COLUMN group_name DROP NOT NULL;
